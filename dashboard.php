838264e290c467e01888e7150295ebc12367f643
mostafijur rahman bipul <34317805+MostafizurRahmanBipul@users.noreply.github.com>
2019-12-19 23:21:47 +0600

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,24 @@
+<?php
+include 'session.php';
+
+$flag = 0;
+if(isset($_POST['addPost'])){
+    
+    //$postDetails = $_POST;
+    $postDetails['category'] = htmlspecialchars($_POST['category']);
+    $postDetails['pdetails'] = htmlspecialchars($_POST['pdetails']);
+    $postDetails['email'] = $email;
+    $res = $db->addNewQuickPost($postDetails);
+    if($res){
+        $flag = 1;
+    }else{
+        $flag = 2;
+    }
+}
+
+$allPosts = $db->getAllPost();
+
+include 'temp/header.php';
+include 'temp/inc_dashboard.php';
+include 'temp/footer.php';
+?>
